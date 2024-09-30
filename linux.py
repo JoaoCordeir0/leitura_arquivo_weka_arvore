dde460b1c0dc2b39f800416727f2c6e2e5d1f1a9
JoaoCordeir0 <joaocordeiro2134@gmail.com>
2024-09-30 13:10:16 -0300

    add

---
+++ linux.py
@@ -0,0 +1,40 @@
+import pandas as pd
+# import arff
+from scipy.io.arff import loadarff
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+def readFile(file_path: str):    
+    # fp = open(file_path)
+    # data = arff.load(fp)    
+
+    # df = pd.DataFrame(data, columns=['Tempo', 'Temperatura', 'Umidade', 'Vento', 'Jogar'])    
+    # return df
+    data = loadarff(file_path)
+    df = pd.DataFrame(data[0], columns=['Tempo', 'Temperatura', 'Umidade', 'Vento', 'Jogar'])
+    return df
+
+def createTree(file_path: str):
+    # Get table datas
+    table_data = readFile(file_path)
+
+    # Divide collumns between the X - attributes and y - result 
+    X = table_data[['Tempo', 'Temperatura', 'Umidade', 'Vento']]
+    y = table_data['Jogar']
+
+    # Hot encode to transform all string values to int, so that the decision tree can read 
+    # them and analyze
+    X = pd.get_dummies(X, columns=['Tempo', 'Temperatura', 'Umidade', 'Vento'], drop_first=True)
+        
+    # Instance DecisionTree class ang give wich criterion will use
+    clf = DecisionTreeClassifier(criterion='entropy')
+    clf = clf.fit(X, y.astype('str'))
+
+    # Generate tree figure
+    plt.figure(figsize=(12,8))
+    tree.plot_tree(clf, filled=True, feature_names=X.columns, class_names=['Nao', 'Sim'], rounded=True)
+    plt.show()
+
+createTree('./model/jogar_tenis.arff')
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Comandos para rodar:
+
+## Linux: 
+> python3 -m venv venv
+> source venv/bin/activate
+> pip install -r requirements.txt
+> python3 main.py
+
+## Windows: 
+> python -m venv venv
+> Venv/Scripts/Activate
+> pip install -r requirements.txt
+> python main.py
